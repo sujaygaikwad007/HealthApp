8a1ea603c7fbed3b475774bf873326d20d2721d7
Aniket Patil <aniket.p@reapmind.com>
2024-06-04 12:16:12 +0530

    Initial Commit

---
+++ HealthApp/HealthAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthAppApp.swift
+//  HealthApp
+//
+//  Created by Aniket Patil on 04/06/24.
+//
+
+import SwiftUI
+
+@main
+struct HealthAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
